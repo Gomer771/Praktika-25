f45307973b27c6e7cdd68847c24a97de03b2cd18
gomer <swistak@tutamail.com>
2025-08-28 16:21:31 +0300

    66

--- 6/manager.py
+++ 6/manager.py
@@ -3,17 +3,19 @@ import random
 import string
 
 def multi_process(shared_dict, process_num):
-    for _ in range(5): #цикл с 5-ю итерациями
-        line = ''.join(random.choices(string.ascii_letters, k=6)) #генирируем случайную строку из 6 букв 
-        print(f" Процесс: {line}")
-
+    for _ in range(5):  #каждый процесс добавляет 5 записей
+        key = f"key_{process_num}_{random.randint(1, 100)}" #создание уникального ключа
+        value = ''.join(random.choices(string.ascii_letters, k=6)) #генерируем случайную строку
+        shared_dict[key] = value #добавлям пару в общий словарь
+        print(f"[Процесс {process_num}] - {key}: {value}")
+        
 def main():
     with multiprocessing.Manager() as manager: #cоздаём менеджер
         shared_dict = manager.dict() #общий словарь для всех процессов
         processes = [] #пустой список
 
-#запуск 5 процесссов
-        for i in range(5):
+#запуск 10 процесссов
+        for i in range(10):
             p = multiprocessing.Process(target=multi_process, args=(shared_dict, i+1))
             processes.append(p)
             p.start()
